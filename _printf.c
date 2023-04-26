2cb2d01b9a5309a2f6659394e4041c02917b6ac7
thebassvoice <hillsjoshua7@gmail.com>
2023-04-26 03:52:31 -0700

    I don't know what i am doing wrong

--- _printf.c
+++ _printf.c
@@ -1,55 +1,58 @@
 #include "main.h"
 
 void print_buffer(char buffer[], int *buff_ind);
+
 /**
  * _printf - Printf function
- * @format: pointer
+ * @format: argument
  * Return: chars.
  */
 int _printf(const char *format, ...)
 {
-	int e, print = 0, print_chars = 0;
-	int f, width, pre, size, buff_ind = 0;
-	va_list l;
+	int i, printed = 0, printed_chars = 0;
+	int flags, width, precision, size, buff_ind = 0;
+	va_list list;
 	char buffer[BUFF_SIZE];
 
 	if (format == NULL)
 		return (-1);
 
-	va_start(l, format);
+	va_start(list, format);
 
-	for (e = 0; format && format[e] != '\0'; e++)
+	for (i = 0; format && format[i] != '\0'; i++)
 	{
-		if (format[e] != '%')
+		if (format[i] != '%')
 		{
-			buffer[buff_ind++] = format[e];
+			buffer[buff_ind++] = format[i];
 			if (buff_ind == BUFF_SIZE)
 				print_buffer(buffer, &buff_ind);
-			/* write(1, &format[i], 1);*/
-			print_chars++;
+			printed_chars++;
 		}
 		else
 		{
 			print_buffer(buffer, &buff_ind);
-			f = get_flags(format, &e);
-			width = get_width(format, &e, l);
-			pre = get_pre(format, &e, l);
-			size = get_size(format, &e);
-			++e;
-			print = handle_print(format, &e, l, buffer,
-				f, width, pre, size);
-			if (print == -1)
+			flags = get_flags(format, &i);
+			width = get_width(format, &i, list);
+			precision = get_precision(format, &i, list);
+			size = get_size(format, &i);
+			++i;
+			printed = handle_print(format, &i, list, buffer,
+				flags, width, precision, size);
+			if (printed == -1)
 				return (-1);
-			print_chars += print;
+			printed_chars += printed;
 		}
 	}
+
 	print_buffer(buffer, &buff_ind);
-	va_end(l);
-	return (print_chars);
+
+	va_end(list);
+
+	return (printed_chars);
 }
 
 /**
- * print_buffer - Function
+ * print_buffer - Main
  * @buffer: argument
  * @buff_ind: pointer
  */
